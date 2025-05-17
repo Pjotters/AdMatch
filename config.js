7e88327b1bfc4dc30d7ab39a2f8161cdb598c9fd
Pjotters-Chain <05.hek_domein@icloud.com>
2025-05-17 20:36:14 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  apiUrl: 'https://cgi-season-procedure-dom.trycloudflare.com', // backend API
+  riffusionUrl: 'https://beautifully-specialties-architectural-tourism.trycloudflare.com' // nieuwe Riffusion API
+};
+window.config = config;
+
